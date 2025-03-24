43dc7e4ba9875737e91dcf0753b85c480f1c6e30
PavleDz <pavledzuverovic@edu.ucg.ac.me>
2025-03-24 13:47:53 +0100

    CNSN-7 RegisterPage.jsx - register page

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,11 @@
+import Register from "../components/auth/Register";
+
+const RegisterPage = () => {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Register />
+    </div>
+  );
+};
+
+export default RegisterPage;
