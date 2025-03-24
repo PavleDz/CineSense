8166402c10345d649f3e98225f7555aa4d5d3ee4
PavleDz <pavledzuverovic@edu.ucg.ac.me>
2025-03-24 13:26:06 +0100

    CNSN-6 LoginPage.jsx - login page

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import Login from "../components/auth/Login";
+
+const LoginPage = () => {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Login />
+    </div>
+  );
+};
+
+export default LoginPage;
