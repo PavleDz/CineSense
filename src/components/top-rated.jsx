814605aeaca2d6ce5767f0176355150e8aafdf83
Andrija Puletic <andrija.puletic@gmail.com>
2025-03-24 14:22:45 +0100

    [CNSN-26] - Initial design of the top rated layout

---
+++ src/components/top-rated.jsx
@@ -0,0 +1,15 @@
+import MovieCard from "./movie-card";
+export default function TopRated() {
+  return (
+    <>
+      <div className="top-rated-container">
+        <MovieCard></MovieCard>
+        <MovieCard></MovieCard>
+        <MovieCard></MovieCard>
+        <MovieCard></MovieCard>
+        <MovieCard></MovieCard>
+        <MovieCard></MovieCard>
+      </div>
+    </>
+  );
+}
