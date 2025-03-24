bdbd5fe327dcc52ceb01b0239e73bd88179f6192
PavleDz <pavledzuverovic@edu.ucg.ac.me>
2025-03-24 13:19:42 +0100

    CNSN-11 Auth.jsx component (used for login/register) pushed

--- src/components/auth/AuthForm.jsx
+++ src/components/auth/AuthForm.jsx
@@ -4,7 +4,7 @@ const AuthForm = ({ title, children, onSubmit }) => {
   return (
     <div className="flex h-screen justify-center items-center bg-gray-100">
       <Paper elevation={4} className="p-8 w-full max-w-md">
-        <Typography variant="h5" className="mb-6 text-center font-bold">
+        <Typography variant="h4" className="mb-6 text-center font-bold">
           {title}
         </Typography>
         <form className="flex flex-col gap-4" onSubmit={onSubmit}>
