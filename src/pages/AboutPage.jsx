0d8c1937b8a8f62841db2445497d900830201160
Dejan Skiljic <skiljic.dejan@gmail.com>
2025-03-25 13:17:44 +0100

    Merge request Dejan (#7)
    * [CNSN-31] - Added support for mobile and fixed flex issues
    
    * [CNSN-35] - Added profile to the header with mobile support
    
    * [CNSN-36] - Added Footer component
    
    * [CNSN-37] - Added routing to the navigation elements in the header and footer
    
    * [CNSN-37] - Added Routing for profile and login pages
    
    * [CNSN-37] - Cleaned up routing by creating MainRoutes component with lazy loading
    
    * [CNSN-40] - Added About us Page
    
    * [CNSN-40] - Fixed overflow issue and adjusted img sizes

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,3 +1,56 @@
+import { Container, Typography, Box } from "@mui/material";
+
 export default function AboutPage() {
-  return <h1>Welcome to About Page</h1>;
+  return (
+    <Container maxWidth="md" sx={{ textAlign: "center", py: 4 }}>
+      <Typography variant="h3" gutterBottom>
+        Welcome to Cinsense.
+      </Typography>
+      <Typography variant="body1" component="p">
+        Your ultimate destination for discovering and keeping track of the
+        latest movies and TV shows. Whether you're searching for new releases,
+        exploring hidden gems, or saving your all-time favorites, Cinsense makes
+        it effortless to manage your watchlist.
+      </Typography>
+      <Box sx={{ display: "flex", justifyContent: "center", gap: 2, my: 2 }}>
+        <img
+          src="src/assets/popcorns.svg"
+          alt="Illustration of the yellow box of popcorns"
+          loading="lazy"
+          maxWidth="100%"
+          height="auto"
+          width="45%"
+        />
+        <img
+          src="src/assets/film-rolls.svg"
+          alt="Illustration of the film rolls"
+          loading="lazy"
+          maxWidth="100%"
+          height="auto"
+          width="45%"
+        />
+      </Box>
+      <Typography variant="body1" component="p">
+        But we don’t stop there—our cutting-edge AI-powered recommendation
+        engine takes your movie and TV show experience to the next level. Based
+        on your favorite picks and search history, Cinsense suggests
+        personalized recommendations, ensuring you never run out of great
+        content to watch.
+      </Typography>
+      <Box sx={{ height: 50 }} />
+      <Typography variant="h6" gutterBottom>
+        Stay ahead of the entertainment curve with Cinsense—where movies and TV
+        shows meet smart discovery!
+      </Typography>
+      <Box sx={{ display: "flex", justifyContent: "center", mt: 4 }}>
+        <img
+          src="src/assets/movie-night.svg"
+          alt="Illustration of the couple on sofa watching TV"
+          loading="lazy"
+          width="100%"
+          height="auto"
+        />
+      </Box>
+    </Container>
+  );
 }
