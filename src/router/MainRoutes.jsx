358e90f0a5e4566fc7403246b72b9cb2cde16900
Pavle Džuverović <pavledzuverovic@edu.ucg.ac.me>
2025-03-25 13:32:12 +0100

    CNSN-46 Routed RegisterPage and AdminPage

--- src/router/MainRoutes.jsx
+++ src/router/MainRoutes.jsx
@@ -8,6 +8,8 @@ const CustomPage = lazy(() => import("../pages/CustomPage"));
 const AboutPage = lazy(() => import("../pages/AboutPage"));
 const ProfilePage = lazy(() => import("../pages/ProfilePage"));
 const LoginPage = lazy(() => import("../pages/LoginPage"));
+const RegisterPage = lazy(() => import("../pages/RegisterPage"));
+const AdminPage = lazy(() => import("../pages/AdminPage"));
 
 export default function MainRoutes() {
   return (
@@ -19,6 +21,8 @@ export default function MainRoutes() {
         <Route path="/about" element={<AboutPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </Suspense>
   );
